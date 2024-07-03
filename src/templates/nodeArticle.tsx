998491af332f905d0d783d4882c14c5dcc87ff69
Cardozo1w <oscar1w2e@gmail.com>
2024-07-02 20:54:06 -0600

    update spacings

--- src/templates/nodeArticle.tsx
+++ src/templates/nodeArticle.tsx
@@ -21,8 +21,8 @@ const NodePage: FC<any> = ({ data: drupal }) => {
         title="Bienvenido a Labfem"
         description={`<p>“Cada paso hacia la igualdad es un paso hacia un mundo mejor.”</p>`}
       />
-      <div className="relative container mx-auto article">
-        <h2 className="text-3xl font-bold text-[#af277d] mt-16 mb-8">
+      <div className="w-[95%] lg:w-full container mx-auto article mt-8 lg:mt-16">
+        <h2 className="text-3xl font-bold text-[#af277d] mb-8">
           {data.title}
         </h2>
         <RichText>{data.fieldBody?.processed}</RichText>
