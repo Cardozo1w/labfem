12f31f52d351436315452e670f023f95c2c72b4b
Cardozo1w <74799921+Cardozo1w@users.noreply.github.com>
2024-09-20 19:14:39 -0600

    update card height

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -9,7 +9,7 @@ const Card: FC<CardProps> = ({ title, image, description, cta }) => {
       <img
         src={image.src}
         alt={image.alt}
-        className="w-full h-48 object-cover"
+        className="w-full h-80 object-cover"
       />
       <div className="p-4">
         <h3 className="text-2xl font-bold text-[#af277d] mb-2">{title}</h3>
